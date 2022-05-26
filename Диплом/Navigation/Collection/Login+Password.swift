b8818d63d3613bad662ab75362a47b3120b439c8
Ninochkaksk <99425053+Ninochkaksk@users.noreply.github.com>
2022-05-27 01:43:41 +0700

    Дипломный блок профессии "iOS-разработчик"
    Дипломный блок профессии "iOS-разработчик"

---
+++ Диплом/Navigation/Collection/Login+Password.swift
@@ -0,0 +1,17 @@
+//
+//  Login+Password.swift
+//  Navigation
+//
+//  Created by Ниночка on 21.05.2022.
+//
+
+import Foundation
+
+struct LoginPassword {
+    let login = "start@gmail.ru"
+    let defaultPassword = "Zxcasd321"
+    let invalid = "Неверный  логин или пароль"
+    let invalidLog = "E-mail введён не корректно"
+    let invalidPass = "Пароль должен быть не менее 8 символов"
+}
+
